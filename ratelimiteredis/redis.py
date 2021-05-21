202d7f3026ac4f4491ea7c720f3402789aa26ca9
PredaaA <46051820+PredaaA@users.noreply.github.com>
2021-05-21 16:37:54 +0200

    0.1.23 - Add password support, and ability to directly pass Redis address.

--- ratelimiteredis/redis.py
+++ ratelimiteredis/redis.py
@@ -1,58 +1,94 @@
 import aioredis
-from typing import NoReturn
+from typing import Optional, NoReturn
 from .exceptions import RedisConnectionFail, RedisMissingConnection
 
 
 class RedisPool:
-    """Represent an Redis pool.
+    """Represents an Redis pool.
 
-    Attributes
+    Parameters
     ----------
-    host: str = "localhost"
-        The Redis host.
-    port: int = 6379
-        The Redis port.
-    db: int = 0
-        The Redis db.
+    address: Optional[str] = None
+        The Redis address
+    host: Optional[str] = "localhost"
+        The Redis host. Will be ignored if address is not None.
+    port: Optional[int] = 6379
+        The Redis port. Will be ignored if address is not None.
+    db: Optional[int] = 0
+        The Redis db. Will be ignored if address is not None.
+    password: Optional[str] = None
+        The Redis password.
     encoding: str = "utf-8"
         The encoding used.
     maxsize: int = 50
         The maximum of DBs of that pool.
     """
 
-    __slots__ = ("_host", "_port", "_db", "_encoding", "_maxsize")
+    __slots__ = ("_address", "_host", "_port", "_db", "_password", "_encoding", "_maxsize")
 
     def __init__(
         self,
-        host: str = "localhost",
-        port: int = 6379,
-        db: int = 0,
+        address: Optional[str] = None,
+        host: Optional[str] = "localhost",
+        port: Optional[int] = 6379,
+        db: Optional[int] = 0,
+        password: Optional[str] = None,
         encoding: str = "utf-8",
         maxsize: int = 50,
     ):
+        self._address = address
         self._host = host
         self._port = port
         self._db = db
+        self._password = password
         self._encoding = encoding
         self._maxsize = maxsize
 
     def __repr__(self):
-        return f"<RedisPool _host={self._host} _port={self._port} _db={self._db} _encoding={self._encoding} _maxsize={self._maxsize}>"
+        return f"<RedisPool _address={self._address} _host={self._host} _port={self._port} _db={self._db} _encoding={self._encoding} _maxsize={self._maxsize}>"
 
 
 class RedisInterface(RedisPool):
+    """Represents the Redis interface to proceed ratelimits.
 
-    __slots__ = ("_redis",)
+    Parameters
+    ----------
+    address: Optional[str] = None
+        The Redis address
+    host: Optional[str] = "localhost"
+        The Redis host. Will be ignored if address is not None.
+    port: Optional[int] = 6379
+        The Redis port. Will be ignored if address is not None.
+    db: Optional[int] = 0
+        The Redis db. Will be ignored if address is not None.
+    password: Optional[str] = None
+        The Redis password.
+    encoding: str = "utf-8"
+        The encoding used.
+    maxsize: int = 50
+        The maximum of DBs of that pool.
+    """
+
+    __slots__ = ("__redis",)
 
     def __init__(self, **kwargs):
-        self._redis = None
+        self.__redis = None
         super().__init__(**kwargs)
 
     async def init_connection(self) -> None:
+        if self._address is None and any(
+            (self._host is None, self._port is None, self._db is None)
+        ):
+            raise RedisConnectionFail(
+                "If address argument is not provided, any of host, port, or db must be passed."
+            )
+
         try:
-            self._redis = await aioredis.create_redis_pool(
-                address=f"redis://{self._host}:{self._port}",
-                db=self._db,
+            address = self._address or f"redis://{self._host}:{self._port}"
+            self.__redis = await aioredis.create_redis_pool(
+                address=address,
+                db=self._db if self._address is None else None,
+                password=self._password,
                 encoding=self._encoding,
                 maxsize=self._maxsize,
             )
@@ -62,16 +98,16 @@ class RedisInterface(RedisPool):
             )
 
     async def teardown(self) -> None:
-        if self._redis:
-            self._redis.close()
+        if self.__redis:
+            self.__redis.close()
 
     @property
     def pool(self) -> aioredis.Redis:
-        if self._redis is None:
+        if self.__redis is None:
             raise RedisMissingConnection(
                 "You need to have an valid Redis connection to use that property."
             )
-        return self._redis
+        return self.__redis
 
     @pool.setter
     async def pool(self, val) -> NoReturn:
